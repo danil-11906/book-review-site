5c0f9431fed3417be91ee7fe8694ca65f6528291
Anvar <anvar00755@mail.ru>
2021-06-21 23:54:50 +0300

    initial

---
+++ src/main/java/com/simbirsoft/practice/bookreviewsite/BookReviewSiteApplication.java
@@ -0,0 +1,13 @@
+package com.simbirsoft.practice.bookreviewsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookReviewSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookReviewSiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/simbirsoft/practice/bookreviewsite/BookReviewSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simbirsoft.practice.bookreviewsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookReviewSiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
